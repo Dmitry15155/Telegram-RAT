ade797940a35ed10f15022c7525409cc2265a40c
Two Brains <77238864+Dmitry15155@users.noreply.github.com>
2022-05-23 01:41:20 +0500

    Add files via upload

---
+++ Modules/Requirements.bat
@@ -0,0 +1,16 @@
+echo off
+pip3 install PyAudio-0.2.11-cp310-cp310-win_amd64.whl
+
+pip3 install pytelegrambotapi
+pip3 install pycryptodome
+pip3 install pyperclip
+pip3 install keyboard
+pip3 install pyttsx3
+pip3 install pywin32
+pip3 install pynput
+pip3 install mss
+
+pip3 install pyinstaller
+
+:cmd
+pause null
